58f552352a27af6bbc3417f0c873bb254ddc3b05
ericknog45 <ericknoglves36@gmail.com>
2026-01-27 20:19:17 -0300

    Diretorio.java

--- src/projetoPoo/Diretorio.java
+++ src/projetoPoo/Diretorio.java
@@ -5,8 +5,23 @@ import java.util.List;
 public class Diretorio extends Componente {
 	private List <Componente> itens;
 	
-	public Diretorio (String nome, Diretorio diretorioPai) {
-		super (nome, diretorioPai);
+	public Diretorio(String nome, Diretorio diretorioPai) {
+		super(nome, diretorioPai);
 		this.itens = new ArrayList<>();
+	}
+	@Override
+	public void exibir() {
+		for(Componente item : itens) {
+			System.out.println(item.getNome());
+		}
+	}
+	public void adicionar(Componente componente) {
+		this.itens.add(componente);
+	}
+	public void remover(Componente componente) {
+		this.itens.remove(componente);
+	}
+	public List<Componente> getItens(){
+		return itens;
 	}
 }

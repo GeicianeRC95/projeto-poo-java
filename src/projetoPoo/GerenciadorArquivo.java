3f1fb09149b76226e1cf9dce147f45f1b4ea6cf8
ericknog45 <ericknoglves36@gmail.com>
2026-01-31 10:04:10 -0300

    Update GerenciadorArquivo.java

--- src/projetoPoo/GerenciadorArquivo.java
+++ src/projetoPoo/GerenciadorArquivo.java
@@ -7,12 +7,26 @@ import java.util.List;
 
 public class GerenciadorArquivo {
 	public void leituraArquivo(Arquivo arquivo) {
-		arquivo.exibir();
+		if (arquivo != null) {
+			arquivo.exibir();
+		}
 	}
-	public void guardar(List<String>historico) {
-		try(FileWriter fw = new FileWriter("historico_terminal.txt");
-		    PrintWriter pw = new PrintWriter(fw)){
-			pw
+
+	public void escreverArquivo(Arquivo arquivo, String texto) {
+		if (arquivo != null) {
+			arquivo.setDados(texto);
+		}
+	}
+
+	public void guardar(List<String> historico) {
+		try (FileWriter fw = new FileWriter("historico_terminal.txt"); PrintWriter pw = new PrintWriter(fw)) {
+			pw.println("+_____HISTÓRICO DE COMANDOS_____+");
+			for (String linha : historico) {
+				pw.println(linha);
+			}
+			System.out.println("Tudo certo! Registro salvo com sucesso.");
+		} catch (IOException excecao) {
+			System.err.println("Opa! Algo deu errado: " + excecao.getMessage());
 		}
 	}
 }

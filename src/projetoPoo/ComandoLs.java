bc42c2f6608b8bb570db6618803d4def2f6b752d
GeicianeRC95 <geicianer.carvalho@gmail.com>
2026-02-01 22:58:00 -0300

    Implement ComandoLs class for directory listing

---
+++ src/projetoPoo/ComandoLs.java
@@ -0,0 +1,22 @@
+package projetoPoo;
+
+public class ComandoLs implements Comando {
+
+    private GerenciadorDiretorio gerenciador;
+
+    public ComandoLs(GerenciadorDiretorio gerenciador) {
+        this.gerenciador = gerenciador;
+    }
+
+    @Override
+    public void executar() {
+        Diretorio atual = gerenciador.getDiretAtual();
+
+        if (atual == null) {
+            System.out.println("Diretório atual não definido.");
+            return;
+        }
+
+        atual.exibir();
+    }
+}

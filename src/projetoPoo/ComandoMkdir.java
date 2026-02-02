1e4da00cce66f9db57a09d2e15a131e126118c28
ericknog45 <ericknoglves36@gmail.com>
2026-02-02 20:03:49 -0300

    Add files via upload

---
+++ src/projetoPoo/ComandoMkdir.java
@@ -0,0 +1,22 @@
+package projetoPoo;
+
+public class ComandoMkdir implements Comando {
+	private GerenciadorDiretorio gerenciador;
+
+	public ComandoMkdir(GerenciadorDiretorio gerenciador) {
+		this.gerenciador = gerenciador;
+	}
+
+	@Override
+	public void executar(String[]args) {
+		if(args.length < 1) {
+			System.out.println("Inválido! É necessário um nome!");
+			return;
+		}
+		String nomePasta = args[0];
+		Diretorio pastaEmExecucao = gerenciador.getDiretAtual();
+		Diretorio novaPasta = new Diretorio(nomePasta, pastaEmExecucao);
+		pastaEmExecucao.adicionar(novaPasta);
+		System.out.println("Novo diretório criado: " + nomePasta);
+	}
+}

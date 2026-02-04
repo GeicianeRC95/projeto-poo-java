3bd048455a0f856c9b50e20c5dfb7ed58bd570c1
GeicianeRC95 <geicianer.carvalho@gmail.com>
2026-02-04 00:37:46 -0300

    Implement ComandoRm class for removing items

--- src/projetoPoo/ComandoRm.java
+++ src/projetoPoo/ComandoRm.java
@@ -1,1 +1,37 @@
-oi
+package projetoPoo;
+
+import java.util.List;
+
+public class ComandoRm implements Comando {
+    private GerenciadorDiretorio gerenciador;
+
+    public ComandoRm(GerenciadorDiretorio gerenciador) {
+        this.gerenciador = gerenciador;
+    }
+
+    @Override
+    public void executar(String[] args) {
+      
+        if (args.length < 1) {
+            System.out.println("rm: falta operando");
+            return;
+        }
+
+        String alvo = args[0];
+        Diretorio pastaEmExecucao = gerenciador.getDiretAtual();
+        Componente itemParaRemover = null;
+
+        for (Componente item : pastaEmExecucao.getItens()) {
+            if (item.getNome().equals(alvo)) {
+                itemParaRemover = item;
+                break;
+            }
+        }
+
+        if (itemParaRemover == null) {
+            System.out.println("rm: Não foi possível remover '" + alvo + "': Não há arquivo ou diretório");
+        } else {
+            pastaEmExecucao.remover(itemParaRemover);
+        }
+    }
+}
